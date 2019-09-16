aa1f9a836fba4d9b649c0f348a30b208ee47f967
Ben Ripkens <bripkens@gmail.com>
2019-09-16 09:30:40 +0200

    Init

---
+++ ui/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur nisi deleniti, odio ab dolorem a eveniet velit at quo voluptas unde placeat repellendus ex in iusto excepturi quam dolor harum!
+    </div>
+  );
+}
